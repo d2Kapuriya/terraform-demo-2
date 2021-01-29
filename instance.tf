ed7be7494dcd2e856cf5bf31af05b3ecd33ca01d
Dixit M. KAPURIYA <17it043@charusat.edu.in>
2021-01-29 13:34:04 +0530

    Add files via upload

---
+++ instance.tf
@@ -0,0 +1,60 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 2.70"
+    }
+  }
+}
+variable "region" {
+default = "ap-south-1"
+}
+
+variable "images" {
+default = "ami-08f63db601b82ff5f"
+}
+
+provider "aws" {
+  profile = "default"
+  region = var.region
+}
+
+variable "env" {}
+
+variable "EC2name" {
+    type = map(any)
+    default = {
+        stage = "ec2-stage-instance"
+        prod = "ec2-prod-instance"
+    }  
+}
+
+resource "aws_instance" "ec2-stage-instance" {
+ami = var.images
+instance_type = "t2.micro"
+user_data = file("user_data.sh")
+
+tags = {
+    Name = "ec2-stage-instance"
+  }
+
+}
+
+
+resource "aws_instance" "ec2-prod-instance" {
+
+    
+ami = var.images
+instance_type = "t2.micro"
+user_data = file("user_data.sh")
+
+tags = {
+    Name = "ec2-prod-instance"
+  }
+
+}
+
+output "Private-ip" {
+  value = var.env == "prod" ? aws_instance.ec2-prod-instance.private_ip : aws_instance.ec2-stage-instance.private_ip
+  description = "ec2-$var.env-instance"
+}
